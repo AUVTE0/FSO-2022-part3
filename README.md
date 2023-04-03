fec6e5b5b60be0a287e72b62dd6e5245e4b8682e
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-03 18:27:16 +0100

    exercise 3.12

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FSO-2022-part3
+App url: http://falling-shadow-6371.fly.dev/
---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+// check for password in cl arg
+if(process.argv.length < 3){
+    console.log('missing password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://fso:${password}@fso.xgrbbds.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+    console.log('phonebook:')
+    Person.find({}).then(result =>{
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+        // process.exit(0)
+    })
+}
+else {
+    const newPerson = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    
+    newPerson.save().then( result => {
+        console.log(`added ${newPerson.name} number ${newPerson.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
+
