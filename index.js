ec82023c855577982bd37064eec97067be643281
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-03 14:33:36 +0100

    exercise 3.4

--- index.js
+++ index.js
@@ -48,9 +48,22 @@ app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
     if(!person){
+        console.log('Person not found!')
         res.status(404).end()
     }
     res.send(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if(!person){
+        console.log('Person not found!')
+        res.status(404).end()
+    }
+    persons = persons.filter(p => p.id !== id)
+    console.log('Success deleting')
+    res.status(204).end()
 })
 
 const PORT = 3001
